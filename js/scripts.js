93d3c00319b3762ab93ae72b448af4f228779f3f
licathyl <83192907+licathyl@users.noreply.github.com>
2021-05-03 17:21:37 -0700

    Add objects to array pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: 'Jigglypuff', height: 5, types: ['fairy', 'normal']},
+  {name: 'Exeggcute', height: 4, types: ['psychic', 'grass']},
+  {name: 'Mr. Mime', height: 10, types: ['psychic', 'fairy']}
+];
