a2ffcd8a3b2459f2baaf3652df966ff5ebc2cb84
licathyl <83192907+licathyl@users.noreply.github.com>
2021-05-06 01:27:55 -0700

    Updated for loop in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
-let pokemonList = [
-  {name: 'Jigglypuff', height: 7, types: ['fairy', 'normal']},
-  {name: 'Exeggcute', height: 4, types: ['psychic', 'grass']},
-  {name: 'Mr. Mime', height: 10, types: ['psychic', 'fairy']}
+// Pokemon repository list
+let repository = [
+  {
+    name: 'Jigglypuff',
+    height: 7,
+    types: ['fairy', 'normal']
+  },
+  {
+    name: 'Exeggcute',
+    height: 4,
+    types: ['psychic', 'grass']
+  },
+  {
+    name: 'Mr. Mime',
+    height: 10,
+    types: ['psychic', 'fairy']
+  }
 ];
-console.log(pokemonList);
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 8 && pokemonList[i].height > 5) {
-    document.write(pokemonList.name + pokemonList.height);
-  } else if (pokemonList[i].height < 5) {
-    document.write(pokemonList.name + pokemonList.height);
-  } else {
-    document.write(pokemonList.name + pokemonList.height);
-    }
+//for loop to display Pokemon names and heights
+
+for (let i = 0; i < repository.length; i++) {
+  document.write(repository[i].name + ' (height: ' + repository[i].height + ')<br>');
 }
