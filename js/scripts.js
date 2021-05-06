b6d8ba2ac386d022a699a9ebec0690bcc397a816
licathyl <83192907+licathyl@users.noreply.github.com>
2021-05-06 00:41:46 -0700

    Add for loop for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 let pokemonList = [
-  {name: 'Jigglypuff', height: 5, types: ['fairy', 'normal']},
+  {name: 'Jigglypuff', height: 7, types: ['fairy', 'normal']},
   {name: 'Exeggcute', height: 4, types: ['psychic', 'grass']},
   {name: 'Mr. Mime', height: 10, types: ['psychic', 'fairy']}
 ];
 console.log(pokemonList);
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 8 && pokemonList[i].height > 5) {
+    document.write(pokemonList.name + pokemonList.height);
+  } else if (pokemonList[i].height < 5) {
+    document.write(pokemonList.name + pokemonList.height);
+  } else {
+    document.write(pokemonList.name + pokemonList.height);
+    }
+}
