82eb630a640a52a8ba95208649f3a43d8017447d
licathyl <83192907+licathyl@users.noreply.github.com>
2021-05-10 15:00:12 -0700

    Add IIFE pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Pokemon repository list
-let repository = [
+let pokemonList = [
   {
     name: 'Jigglypuff',
     height: 0.7,
@@ -17,8 +17,30 @@ let repository = [
   }
 ];
 
-//forEach loop to Pokemonr epository
-repository.forEach(function(pokemon) {
+// IIFE for pokemonList called pokemonRepository
+let pokemonRepository = (function() {
+  let pokemonList = [];
+
+  function add() {
+    pokemonList.push(item);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+});
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Charmander'});
+console.log(pokemonRepository.getAll());
+
+//forEach loop to display name & height from Pokemon repository list
+pokemonList.forEach(function getAll(pokemon) {
   if (pokemon.height > 0.9 ) {
     document.write(pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!<br><br>');
   } else {
