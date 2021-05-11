f1fe08857824e980e2626aa8366427a5181b80b5
licathyl <83192907+licathyl@users.noreply.github.com>
2021-05-11 12:45:07 -0700

    Update IIFE for pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
-// Pokemon repository list
-let pokemonList = [
-  {
-    name: 'Jigglypuff',
-    height: 0.7,
-    types: ['fairy', 'normal']
-  },
-  {
-    name: 'Exeggcute',
-    height: 0.4,
-    types: ['psychic', 'grass']
-  },
-  {
-    name: 'Mr. Mime',
-    height: 1,
-    types: ['psychic', 'fairy']
-  }
-];
-
 // IIFE for pokemonList called pokemonRepository
 let pokemonRepository = (function() {
-  let pokemonList = [];
+// Pokemon repository list
+  let pokemonList = [
+    {
+      name: 'Jigglypuff',
+      height: 0.7,
+      types: ['fairy', 'normal']
+    },
+    {
+      name: 'Exeggcute',
+      height: 0.4,
+      types: ['psychic', 'grass']
+    },
+    {
+      name: 'Mr. Mime',
+      height: 1,
+      types: ['psychic', 'fairy']
+    }
+  ];
 
   function add(item) {
     pokemonList.push(item);
@@ -36,11 +34,11 @@ let pokemonRepository = (function() {
 })();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({name: 'Charmander'});
+pokemonRepository.add({name: 'Wooper', height: 0.4, types: ['water', 'ground']});
 console.log(pokemonRepository.getAll());
 
-//forEach loop to display name & height from Pokemon repository list
-pokemonList.forEach(function getAll(pokemon) {
+//forEach loop to fetch information from new IIFE pokemonRepository
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height > 0.9 ) {
     document.write(pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!<br><br>');
   } else {
